373e6f4f698252b46f6cb845419a4debb49a211c
kotb 18 <aahmedkotb2498@gmail.com>
2026-03-15 21:01:28 +0200

    edit

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,12 +8,8 @@ import FirebaseCore
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    // 1. تهيئة Firebase أولاً
     FirebaseApp.configure()
-    
-    // 2. تسجيل إضافات Flutter (هذا السطر جوهري لربط FirebaseCore بـ Xcode)
     GeneratedPluginRegistrant.register(with: self)
-    
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
